678a4294af394c692d5fe8293c7c0c248f39b1f7
MK-coder <52727959+MK-coder@users.noreply.github.com>
2019-08-07 06:13:17 -0400

    Add files via upload

---
+++ assets/data/thebus.ts
@@ -0,0 +1,14 @@
+import { BusSchedule } from '../../app/busschedule';
+
+export const  BUSES: BusSchedule [] = [
+    {
+        route: 'BRAMPTON',
+        timeleave: '18:00',
+        timearrive: '23:00'
+    },
+    {
+        route: 'OAKVILLE',
+        timeleave: '19:00',
+        timearrive: '20:00'
+    }
+]; 
